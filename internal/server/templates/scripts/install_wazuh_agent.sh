7b56390ff1ec336573be646121fbc951edec0778
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2026-05-09 19:29:15 +0300

    Change name of wazuh agent to machine id.

--- internal/server/templates/scripts/install_wazuh_agent.sh
+++ internal/server/templates/scripts/install_wazuh_agent.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 install_wazuh_agent() {
   local wazuh_manager={{ .WazuhManger }}
   local wazuh_agent_group={{ .WazuhAgentGroup }}
-  local wazuh_agent_name={{ .WazuhAgentName }}
+  local machine_id={{ .MachineId }}
 
   wget -O /tmp/wazuh.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.14.4-1_amd64.deb 
-  WAZUH_MANAGER="${wazuh_manager}" WAZUH_AGENT_GROUP="${wazuh_agent_group}" WAZUH_AGENT_NAME="${wazuh_agent_name}" dpkg -i /tmp/wazuh.deb
+  WAZUH_MANAGER="${wazuh_manager}" WAZUH_AGENT_GROUP="${wazuh_agent_group}" WAZUH_AGENT_NAME="${machine_id}" dpkg -i /tmp/wazuh.deb
   rm -rf /tmp/wazuh.deb
 }
 
